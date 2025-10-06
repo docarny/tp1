75070a0c0d32daa8a35aca420ecc812568b0417c
docarny <72956074+docarny@users.noreply.github.com>
2025-10-06 12:46:59 -0400

    le code pour trouver lunite fonctionne

--- essaie 2.py
+++ essaie 2.py
@@ -1,11 +1,32 @@
 def racine(nombre):
- for i in range(nombre):
-  if i*i > nombre:
-     break
-    elif i*i == nombre :
-    break
+    resultat = 0.0
+    for i in range(nombre):  #donne une valeur de 0 a "nombre" pour i
+        if i*i == nombre:   #si i^2 est egale a "nombre alors la fonction retourne i comme reponse
+         return i
+        elif i*i > nombre : #si i^2 est plus grand alors la fontion adition (i-1) a resultat, ce qui donne lunite de la racine
+            resultat = i-1 + resultat
+            return resultat #la fonction retourne le resultat et donc lunite de la racine
 
-print(racine(1))
-print(racine(8))
-print(racine(4))
-print(racine(9))
+def decimale(n, nombre):
+    valeur = racine(nombre)
+    etape = 0.1
+
+    for d in range(n):
+
+        if
+
+
+
+
+
+
+
+
+
+
+
+print(racine(3))
+#print(racine(4))
+#print(racine(9))
+#print(racine(25))
+#print(racine(64))
